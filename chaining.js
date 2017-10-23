df506a33b2ec9916fdac1bc50aa60d91a0d85e0c
Kristen Norris <krnorris65@gmail.com>
2017-10-23 16:05:31 -0500

    added number sort and filter

--- chaining.js
+++ chaining.js
@@ -1,6 +1,20 @@
 const integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
 
 // Sort the numbers in descending order (10, 9, 8, 7, etc).
+const sortNums = function(first, second) {
+    return second - first
+}
+
+integers.sort(sortNums)
+
 // Remove any integers greater than 19.
+const under19 = integers.filter(function(num) {
+    if (num < 19) {
+    return num
+    }
+})
+
 // Multiply each remaining number by 1.5 and then subtract 1.
+forEach
+
 // Then output (either in the DOM or the console) the sum of all the resulting numbers.
